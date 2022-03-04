bdbdd9fc901464767c54971157a82c160810bed1
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-04 03:56:18 +0200

    start downloadSnippets function

---
+++ src/downloadSnippets.ts
@@ -0,0 +1,12 @@
+/**
+ * @function downloadSnippets
+ * @param localSnippetsDir {string} Path to the local snippets directory
+ * @param fileUrls {string[]} List of URLs of snippet JSON files to download
+ */
+export const downloadSnippets = (localSnippetsDir: string, fileUrls: string[]) => {
+  console.log('downloadSnippets() called', { fileUrls, localSnippetsDir });
+
+  let snippetContent = [];
+
+  
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {downloadSnippets} from './downloadSnippets';
+
+const path = require('path');
+// const snippetsPath = path.resolve(vscode.env.appRoot, 'snippets');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('shalom world, "snippet-sync-vscode" is now active');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "team-snippets" is now active!');
+	const snippetsDir = path.resolve(context.globalStorageUri.path, '../..', 'snippets');
+	const snippetFilesList = vscode.workspace.getConfiguration('snippetSync').get<string[]>('snippetFiles') || [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('team-snippets.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Team Snippets!');
+	let disposable = vscode.commands.registerCommand('snippet-sync-vscode.updateAllSnippetFiles', () => {
+		console.log('"snippet-sync-vscode.updateAllSnippetFiles" command called');
+		downloadSnippets(snippetsDir, snippetFilesList);
 	});
 
 	context.subscriptions.push(disposable);

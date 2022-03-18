8e8abe751cb8d75c7af8e42ce1e03fe300a87a36
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-18 04:28:54 +0200

    fix types and logging

--- src/downloadSnippets.ts
+++ src/downloadSnippets.ts
@@ -1,12 +1,52 @@
+const axios = require('axios');
+
+type AxiosHttpResponse = {
+    status: string | number,
+    statusText: string,
+    data: any
+};
+
+type HttpResult = {
+  status: 'success' | 'fail',
+  data?: object,
+  reason?: string,
+  error?: any
+};
+
 /**
  * @function downloadSnippets
  * @param localSnippetsDir {string} Path to the local snippets directory
  * @param fileUrls {string[]} List of URLs of snippet JSON files to download
  */
-export const downloadSnippets = (localSnippetsDir: string, fileUrls: string[]) => {
+export const downloadSnippets = async (localSnippetsDir: string, fileUrls: string[]): Promise<void> => {
   console.log('downloadSnippets() called', { fileUrls, localSnippetsDir });
+  let statuses: object[] = [];
 
-  let snippetContent = [];
+  for (const fileUrl of fileUrls) {
+    const response = await fetchFile(fileUrl);
+    statuses.push(response);
+  }
 
-  
+  console.log('Statuses for retrieved files:', statuses);
+};
+
+const fetchFile = async (url: string): Promise<HttpResult> => {
+  console.log(`fetchFile() called for ${url}`);
+
+  try {
+    const response: AxiosHttpResponse = await axios.get(url);
+    console.log(`Response received for ${url}:`, {response});
+
+    if (!response?.data) {
+      return { status: 'fail', reason: 'no_data_in_response' };
+    }
+
+    if (typeof response.data !== 'object') {
+      return { status: 'fail', reason: 'data_not_object' };
+    }
+
+    return { status: 'success', data: response.data };
+  } catch (error) {
+    return { status: 'fail', reason: 'error', error };
+  }
 };

a779549e2e40ea8d5073644ef6f0f3331d6b4020
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-20 00:29:37 +0200

    prepend + append snippet filenames

--- src/convertUrlToFilename.ts
+++ src/convertUrlToFilename.ts
@@ -1,11 +1,17 @@
+const prefix = 'snippet-sync__';
+const extension = '.code-snippets';
+
 export function convertUrlToFilename(url: string): string {
-  const filename = encodeURIComponent(url) + '.code-snippets';
+  const filename = prefix + encodeURIComponent(url) + extension;
   console.debug('Converted URL to filename:', { url, filename });
   return filename;
 }
 
 export function convertFilenameToUrl(filename: string): string {
-  const url = decodeURIComponent(filename.replace(/\.code-snippets$/m, ''));
+  const strippedFilename = filename
+    .replace(new RegExp(`^${prefix}`, 'm'), '')
+    .replace(new RegExp(`${extension}$`, 'm'), '');
+  const url = decodeURIComponent(strippedFilename);
   console.debug('Converted filename to URL:', { filename, url });
   return url;
 }

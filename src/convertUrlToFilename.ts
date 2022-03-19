363af8981c51fb678f448f14ccfdd2f6dfbcb853
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-20 00:17:12 +0200

    add .code-snippets to local filenames

--- src/convertUrlToFilename.ts
+++ src/convertUrlToFilename.ts
@@ -1,11 +1,11 @@
 export function convertUrlToFilename(url: string): string {
-  const filename = encodeURIComponent(url);
+  const filename = encodeURIComponent(url) + '.code-snippets';
   console.debug('Converted URL to filename:', { url, filename });
   return filename;
 }
 
 export function convertFilenameToUrl(filename: string): string {
-  const url = decodeURIComponent(filename);
+  const url = decodeURIComponent(filename.replace(/\.code-snippets$/m, ''));
   console.debug('Converted filename to URL:', { filename, url });
   return url;
 }

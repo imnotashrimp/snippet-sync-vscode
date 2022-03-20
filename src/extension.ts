c9fcda15b97e891399a198a5f59040d66f9e0352
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-20 09:08:01 +0200

    finalize success and fail reporting

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import {retrieveSnippets} from './retrieveSnippets';
 import { clearSnippets } from './clearSnippetFiles';
 import { writeSnippetFiles } from './writeSnippetFiles';
+import { WriteSuccessResult, HttpFailResult, HttpErrorResult, WriteFailResult } from './types';
 
 const path = require('path');
 
@@ -17,13 +18,34 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log('"snippet-sync-vscode.updateAllSnippetFiles" command called');
 		clearSnippets(snippetsDir);
 
-		const retrievedSnippets = await retrieveSnippets(snippetsDir, snippetFilesList);
-		vscode.window.showWarningMessage(`${retrievedSnippets.fails.length} snippet files can't be retrieved:
-			${retrievedSnippets.fails.map(failedFile => failedFile.url).join(', \n')}`
-		);
+		const httpFetchResults = await retrieveSnippets(snippetsDir, snippetFilesList);
+		const writeResults = writeSnippetFiles(snippetsDir, httpFetchResults.successes);
 
-		writeSnippetFiles(snippetsDir, retrievedSnippets.successes);
-		// TODO pop success message for written files
+		// Build success & fail reporting arrays
+		const allSuccesses: WriteSuccessResult[] = writeResults.successes;
+		const allFails: (HttpFailResult|HttpErrorResult|WriteFailResult)[] = [...httpFetchResults.fails, ...writeResults.fails];
+
+		// Pop success & failure messages, and log full objects
+		if (allFails.length > 0) {
+			const fileOrFiles = allFails.length === 1 ? 'file' : 'files';
+
+			vscode.window.showWarningMessage(
+				`${allFails.length} snippet ${fileOrFiles} couldn't be updated.
+				Please check that the ${fileOrFiles} can be accessed without authentication
+				and that ${allFails.length === 1 ? 'it contains' : 'they contain'} valid JSON.
+				To stop seeing this message, remove the ${fileOrFiles} from the Snippet Sync config:
+				${allFails.map(fail => fail.url).join(', ')}`
+			);
+		}
+
+		if (allSuccesses.length > 0) {
+			vscode.window.showInformationMessage(
+				`${allSuccesses.length} snippet ${allSuccesses.length === 1 ? 'file was' : 'files were'} updated:
+				${allSuccesses.map(success => success.url).join(', ')}`
+			);
+		}
+
+		console.log('snippet-sync update results:', {allSuccesses, allFails});
 	});
 
 	context.subscriptions.push(disposable);

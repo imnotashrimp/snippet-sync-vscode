b0ad34704cdd84626ef8e4332f3bfe0114edeb41
Stefan (Shalom) <imnotashrimp@users.noreply.github.com>
2022-03-25 04:56:58 +0300

    filenames (#8)
    * delete convertFilenameToUrl (unused)
    
    * change local snippet filename format

--- src/paths/filenames.ts
+++ src/paths/filenames.ts
@@ -1,16 +1,9 @@
 import config from '../config';
+import { parseGitHubUrl } from './gitHubUrl';
 
 export function convertUrlToFilename(url: string): string {
-  const filename = config.filePrefix + encodeURIComponent(url) + config.fileExtension;
+  const { owner, repo, path } = parseGitHubUrl(url);
+  const filename = config.filePrefix + `${owner}_${repo}_${path.replace('/', '_')}` + config.fileExtension;
   console.debug('Converted URL to filename:', { url, filename });
   return filename;
-}
-
-export function convertFilenameToUrl(filename: string): string {
-  const strippedFilename = filename
-    .replace(new RegExp(`^${config.filePrefix}`, 'm'), '')
-    .replace(new RegExp(`${config.fileExtension}$`, 'm'), '');
-  const url = decodeURIComponent(strippedFilename);
-  console.debug('Converted filename to URL:', { filename, url });
-  return url;
 }

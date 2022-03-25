cc41bf753fe3a03ae9ea99da94860e7f595e8a63
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-25 04:55:53 +0300

    delete convertFilenameToUrl (unused)

--- src/paths/filenames.ts
+++ src/paths/filenames.ts
@@ -5,12 +5,3 @@ export function convertUrlToFilename(url: string): string {
   console.debug('Converted URL to filename:', { url, filename });
   return filename;
 }
-
-export function convertFilenameToUrl(filename: string): string {
-  const strippedFilename = filename
-    .replace(new RegExp(`^${config.filePrefix}`, 'm'), '')
-    .replace(new RegExp(`${config.fileExtension}$`, 'm'), '');
-  const url = decodeURIComponent(strippedFilename);
-  console.debug('Converted filename to URL:', { filename, url });
-  return url;
-}

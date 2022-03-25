02e16f159eb93bad3165f1327bd9cab1490de30a
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-03-25 04:56:30 +0300

    change local snippet filename format

--- src/paths/filenames.ts
+++ src/paths/filenames.ts
@@ -1,7 +1,9 @@
 import config from '../config';
+import { parseGitHubUrl } from './gitHubUrl';
 
 export function convertUrlToFilename(url: string): string {
-  const filename = config.filePrefix + encodeURIComponent(url) + config.fileExtension;
+  const { owner, repo, path } = parseGitHubUrl(url);
+  const filename = config.filePrefix + `${owner}_${repo}_${path.replace('/', '_')}` + config.fileExtension;
   console.debug('Converted URL to filename:', { url, filename });
   return filename;
 }
